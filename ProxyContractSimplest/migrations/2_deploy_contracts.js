f26e8085a7fd988d3301b18baacf0dfd580b06ff
Michael Osborne <resonatortune@gmail.com>
2018-10-07 15:52:33 -0700

    Removed ProxyContract simple, in favor of ProxyContractERC20

--- ProxyContractSimplest/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var Proxy = artifacts.require("Proxy");
-var Logic1 = artifacts.require("Logic1");
-
-module.exports = function(deployer) {
-    deployer.deploy(Proxy).then(function() {
-        return deployer.deploy(Logic1, Proxy.address);
-    });
-};
--- ProxyContractSimplest/migrations/3_deploy_logic2.js
+++
@@ -1,6 +0,0 @@
-var Proxy = artifacts.require("Proxy");
-var Logic2 = artifacts.require("Logic2");
-
-module.exports = function(deployer) {
-    deployer.deploy(Logic2, Proxy.address);
-};
